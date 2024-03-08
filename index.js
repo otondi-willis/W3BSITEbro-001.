eeb471aae86f01bf16e7d154088db6cbf1681ec7
Willis Otondi <willis.otondi.ke@gmail.com>
2024-03-08 04:38:05 +0300

    random number generator

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-//Math = built in object that provides a 
-//collection of properties and methods
+//RANDOM NUMBER GENERATOR
 
-let x = 3.21;
-let y = 2;
-let z;
+const myButton = document.getElementById('myButton');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
 
-z = Math.floor(x);
-z = Math.round(x);
-z = Math.ceil(x);
-z = Math.trunc(x);
-z = Math.pow(x, 2);
-z = Math.sqrt(x);
-z = Math.log(x);
-
-
-console.log(z);
+const min = 1;
+const max =6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+myButton.onclick = function () {
+    randomNum1 = Math.floor(Math.random() * max ) + min;
+    label1.textContent = randomNum1;
+    randomNum2 = Math.floor(Math.random() * max ) + min;
+    label2.textContent = randomNum2;
+    randomNum3 = Math.floor(Math.random() * max ) + min;
+    label3.textContent = randomNum3;
+}
